67397d9c9f55044c35878003730200cc4c7ed8ce
Kevin Ngo <xngokevin@yahoo.com>
2016-11-03 19:18:00 -0700

    Refractor, add main page

---
+++ app/src/main/java/overwatch/app/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package overwatch.app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import overwatch.app.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
